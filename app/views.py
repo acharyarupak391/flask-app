d43a17d17b8e439a3dfec9a22730af8ab6e43d14
Rupak Acharya <acharyarupak391@gmail.com>
2020-07-22 16:10:26 +0545

    mongodb connect

--- app/views.py
+++ app/views.py
@@ -1,5 +1,11 @@
 from app import app
 from flask import render_template
+from app.utils.database import DB
+db = DB()
+
+@app.errorhandler(404)
+def error_handler(e):
+  return "Oops! Seems like the page you're trying to get doesn't exist.", 404
 
 @app.route('/')
 def hello():
