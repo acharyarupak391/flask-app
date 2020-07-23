25e017aadd25225c943862ff480af588a0362223
Rupak Acharya <acharyarupak391@gmail.com>
2020-07-23 11:25:29 +0545

    added secrets.py

--- app/utils/database.py
+++ app/utils/database.py
@@ -1,7 +1,9 @@
 from pymongo import MongoClient
+from secrets import mongo_user, mongo_pass
 
 def connect(db_name='flask'):
-  client = MongoClient("mongodb+srv://newdbuser:I9hXhBTUMSOpNCSl@flask-app-cluster.dtoxs.gcp.mongodb.net/?retryWrites=true&w=majority")
+  mongo_string = "mongodb+srv://"+mongo_user+":"+mongo_pass+"@flask-app-cluster.dtoxs.gcp.mongodb.net/?retryWrites=true&w=majority"
+  client = MongoClient()
   db = client[db_name]
   print('connected to database: ', db_name)
   return db
